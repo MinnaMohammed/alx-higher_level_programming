6ae0d9ca0be1f1b4157138f9bf4918759cf5c091
MinnaMohammed <barakatminna1@gmail.com>
2023-08-09 19:11:41 -0400

    python script

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+newNum = number % 10
+if newNum > 5
+    print(f"Last digit of {number:d} is {newNum:d} and is greater than 5")
+elif
+    print(f"

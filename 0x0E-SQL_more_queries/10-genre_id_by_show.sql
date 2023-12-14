3143214d658a0d54c04a0e8c263b82c0c13c3fea
MinnaMohammed <barakatminna1@gmail.com>
2023-12-13 18:40:10 -0800

    [SQL - More queries] - task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- A script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title AS title tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres
+JOIN tv_show_genres.genre_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

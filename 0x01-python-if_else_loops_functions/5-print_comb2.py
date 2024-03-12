5ce2761fa3e4d1aa4ac0cb61e84f3377fb91c150
MinnaMohammed <barakatminna1@gmail.com>
2024-03-12 12:57:54 -0700

    [5. 00...99]

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    if num == 0:
+        comma = ""
+    if num < 10:
+        zero = '0'
+    else:
+        zero = ''
+    print("{}{:s}{:d}".format(comma,zero,num), end="")
+    comma =", "
+print("{:s}".format('\n'))

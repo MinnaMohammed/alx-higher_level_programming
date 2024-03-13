bdfae8d38f3847e235e86a4f5ee3d11c1f1b23ee
MinnaMohammed <barakatminna1@gmail.com>
2024-03-12 18:19:43 -0700

    [11. a ^ b]

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
+    result = 1
     if b < 0:
         power = b * -1
     else:
         power = b
-    for i in range(0, power - 1):
-        a *= a
+    for i in range(power):
+        result *= a
     if power != b:
-        return 1 / a
+        return 1 / result
     else:
-        return a
+        return result

a08987c68b03de4905a90c1906338653b7fbdc0e
MinnaMohammed <barakatminna1@gmail.com>
2024-03-14 19:59:33 -0700

    [3. Infinite addition]

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def add_arguments(args):
+    total = 0
+    for arg in args:
+        total += int(arg)
+    return total
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    result = add_arguments(arguments)
+    print(result)

25c6ded3cb3f6d202d5e49cf46ff6a671ae53fe0
MinnaMohammed <barakatminna1@gmail.com>
2024-03-19 19:12:58 -0700

    Special Task: [13. Linked list palindrome]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,7 +9,24 @@
 */
 int is_palindrome(listint_t **head)
 {
+	listint_t *current;
+	unsigned int n; /* number of nodes */
+
+	current = *head;
+
 	if (*head == NULL)
 		return (1);
+
+	n = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		n++;
+	}
+
+	if (n % 2 == 0)
+		return (1);
+	else
+		return (0);
 	return (1);
 }

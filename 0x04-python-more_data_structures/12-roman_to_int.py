a0aba1d5b2871afe833f35ef38953a90c65e4b5a
MinnaMohammed <barakatminna1@gmail.com>
2024-03-20 16:48:25 -0700

    Special Task: [12. Roman to Integer]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,14 @@
 def roman_to_int(roman_string):
     if isinstance(roman_string, str) is not True or roman_string is None:
         return 0
-    dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     summ = 0
+    prev = ''
     for char in roman_string:
         if char in dic:
-            summ += dic[char]
+            if prev != '' and dic[char] > dic[prev]:
+                summ += dic[char] - 2*(dic[prev])
+            else:
+                summ += dic[char]
+            prev = char
     return (summ)

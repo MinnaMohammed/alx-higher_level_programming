01234432da58475d18e571297cb8cbed6d03ccde
MinnaMohammed <barakatminna1@gmail.com>
2024-03-20 07:45:15 -0700

    [1. Search and replace]

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = list(my_list)
-    new_list.insert(search, replace)
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = my_list[i]
+
     return new_list

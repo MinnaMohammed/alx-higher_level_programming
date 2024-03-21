f0240f09df6c32d0bf8766f8f9cb99ad0ee59731
MinnaMohammed <barakatminna1@gmail.com>
2024-03-20 17:54:54 -0700

    Optional Task: [13. Weighted average]

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,11 @@ def weight_average(my_list=[]):
     summ = 0
     den = 0
     for i in my_list:
+        if i[1] == '':
+            i[1] = 0
+        elif i[0] == '':
+            i[0] = 0
+
         summ += (i[0] * i[1])
         den += i[1]
     return summ/den

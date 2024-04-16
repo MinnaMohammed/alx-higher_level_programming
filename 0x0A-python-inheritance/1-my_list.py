6104e821549fc8d24f0cf8083de6b06094760876
MinnaMohammed <barakatminna1@gmail.com>
2024-04-15 17:42:47 -0700

    [1. My list]

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Python Module'''
+
+class MyList(list):
+    '''MyList class
+
+    Public instance method:
+        def print_sorted(self): Prints the list sorted in ascending order.
+    '''
+
+    def print_sorted(self):
+        '''A function that sorts a list'''
+        sorted_list = sorted(self)
+        return(sorted_list)

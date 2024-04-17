725c5e109f3d210ad3c9f31b26bcc51efb1eb003
MinnaMohammed <barakatminna1@gmail.com>
2024-04-17 07:53:38 -0700

    [4. Only sub class of]

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
     Returns:
         bool: True if obj inherits from a_class, otherwise False
     '''
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

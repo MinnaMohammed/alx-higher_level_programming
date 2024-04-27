bc0607fefc52237b1e54eefc5f72d26eb9de6c83
MinnaMohammed <barakatminna1@gmail.com>
2024-04-26 17:31:16 -0700

    [17. JSON string to dictionary]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         '''A method that returns the list of the JSON string representation'''
-        if json_string is None or len(json_string) == 0:
+        if not json_string:
             return []
         else:
             json.loads(json_string)

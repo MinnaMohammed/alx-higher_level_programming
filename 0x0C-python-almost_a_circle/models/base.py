1ceb900a41f4babdbd84adac6f2370ef9139e4f6
MinnaMohammed <barakatminna1@gmail.com>
2024-04-26 17:03:52 -0700

    [15. Dictionary to JSON string]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''' Python Module '''
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        ''' A method that returns the JSON string representation of a dict '''
+        if list_dictionaries is not None or len(list_dictionaries) != 0:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"

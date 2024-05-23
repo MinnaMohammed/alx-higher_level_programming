e82621f706880633df79e4162c9a3fc60b7126e7
MinnaMohammed <barakatminna1@gmail.com>
2024-05-22 18:56:20 -0700

    [10. Genre ID by show]

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

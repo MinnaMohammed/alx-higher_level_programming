21ba86e1854a03a2e42bc36bc06728a954e31875
MinnaMohammed <barakatminna1@gmail.com>
2024-05-22 19:15:16 -0700

    [16. List shows and genres]

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,5 @@
 -- A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-SELECT
-    tv_shows.title,
-    (SELECT GROUP_CONCAT(tv_genres.name SEPARATOR ', ')
-     FROM tv_genres
-     JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-     WHERE tv_show_genres.show_id = tv_shows.id) AS genres
-FROM
-    tv_shows
-ORDER BY
-    tv_shows.title ASC;
-
+SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre
+FROM tv_shows
+LEFT JOIN tv_genres ON tv_shows.tv_genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, genre ASC;

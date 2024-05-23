57a06085cdf5dd80983ebde5e609890cf8b88e0f
MinnaMohammed <barakatminna1@gmail.com>
2024-05-22 19:27:19 -0700

    Optional Task: [20. Best genre]

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- A script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(rating) AS rating_sum
+FROM tv_genres
+JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;

e2ebcba823b008913e285c427178e2cd7b163038
MinnaMohammed <barakatminna1@gmail.com>
2024-05-23 18:41:26 -0700

    Optional Task: [19. Rotten tomatoes]

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,5 @@
 -- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT title, SUM(rating) AS rating_sum
-FROM hbtn_0d_tvshows_rate
+FROM tv_show_ratings
 GROUP BY title
 ORDER BY rating_sum DESC;
-

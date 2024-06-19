0282365755f85ef8a1e010684a36afb55a9193d4
MinnaMohammed <barakatminna1@gmail.com>
2024-06-18 20:11:49 -0700

    [8. Square]

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-
 const size = process.argv[2];
-
+const n = parseInt(size, 10);
 if (isNaN(size) || size === undefined) {
   console.log('Missing size');
 } else {
-  const n = parseInt(size, 10);
-  if (n <= 0) {
-  } else {
-    for (let i = 0; i < n; i++) {
-      let row = '';
-      for (let j = 0; j < n; j++) {
-        row += 'X';
-      }
-      console.log(row);
+  for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j < n; j++) {
+      row += 'X';
     }
+    console.log(row);
   }
 }

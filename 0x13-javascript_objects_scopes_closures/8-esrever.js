be9b9ad1ec0dc5c3cc206cdc705f136081e88054
MinnaMohammed <barakatminna1@gmail.com>
2024-06-19 19:13:49 -0700

    [8. Esrever]

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const newList = [];
   let cnt = 0;
-  for (let i = list.length; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     newList[i] = list[cnt];
     cnt++;
   }

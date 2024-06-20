001c1975b8b028abb2612ccf558b337d1e160221
MinnaMohammed <barakatminna1@gmail.com>
2024-06-19 19:03:05 -0700

    [6. Square #1]

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const firstSquare = require('./5-square');
+class Square extends firstSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+module.exports = Square;

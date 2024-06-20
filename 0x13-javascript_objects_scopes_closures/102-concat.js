0cb5748b60b9f765e94eacf9012f7858a5b9d782
MinnaMohammed <barakatminna1@gmail.com>
2024-06-19 19:43:42 -0700

    Optional Task: [13. Concat files]

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file1Content = fs.readFileSync(process.argv[2], 'utf8');
-const file2Content = fs.readFileSync(process.argv[3], 'utf8');
+const file1Content = fs.readFileSync(process.argv[2]);
+const file2Content = fs.readFileSync(process.argv[3]);
 const concatenatedContent = file1Content + file2Content;
 
 fs.writeFileSync(process.argv[4], concatenatedContent);
-

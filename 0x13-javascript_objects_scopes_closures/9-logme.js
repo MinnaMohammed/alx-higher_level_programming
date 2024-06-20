0984880509790935434378ec3f534b4961514ef9
MinnaMohammed <barakatminna1@gmail.com>
2024-06-19 19:23:16 -0700

    [9. Log me]

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+//Global scope
+let argsCnt = 0;
 exports.logMe = function (item) {
-  let argsCnt = 0;
-    console.log(argsCnt + ': ' + ${item});
+    console.log(`${argsCnt}: ${item}`);
     argsCnt++;
   }
 };
